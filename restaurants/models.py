ffc5594e676085418b2061b7fc9bdecd789d1499
Yousef Alsaeid <macshiba@mac.com>
2018-11-26 20:35:06 +0300

    first submission

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=80)
+	description = models.TextField()
+	opening_time = models.TimeField()
+	closing_time = models.TimeField()
+
+	def __str__(self):
+		return self.name
